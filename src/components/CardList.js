6560cb695e41ba38b8e2b6e7bce044f7c7c115b3
Milton Chang <mchangpi@gmail.com>
2020-07-29 23:17:50 +0800

    Add ErrorBoundary component

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,6 +2,9 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
+  if (true) {
+    throw new Error("NOOOOOO!");
+  }
   const cardList = robots.map((robot, idx) => {
     return <Card key={idx} robot={robot} />;
   });
