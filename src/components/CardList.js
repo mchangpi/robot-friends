fcf1708c505dceb15b8e622ddefa04001d4509ac
Milton Chang <mchangpi@gmail.com>
2020-07-29 23:26:18 +0800

    Comment out throwing errors

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
-  if (true) {
+  if (0) {
     throw new Error("NOOOOOO!");
   }
   const cardList = robots.map((robot, idx) => {
